96b883f359243e4f65630d89b53228620c196aef
aaaawheee <raviprajapati4925@gmail.com>
2025-04-17 01:23:46 -0400

    upated Backedn URL for Frontend use

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import GroceryList from './components/GroceryList';
 import type { Recipe } from './types';
 import AddNewRecipe from './components/AddNewRecipe';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   // state management
